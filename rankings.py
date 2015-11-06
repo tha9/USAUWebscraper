6714fadd21875e35769837454efbf8f8d6f02a2c
Anthony Dario <anthonyrdario@gmail.com>
2015-11-05 21:30:18 -0500

    Initial commit
    gets the top 20 teams for college. puts it into a .csv file, not
    sure whats up with the printing though

---
+++ rankings.py
@@ -0,0 +1,66 @@
+import requests
+import re
+import unicodedata
+import urllib
+import csv
+from bs4 import BeautifulSoup
+
+# html = requests.get("http://play.usaultimate.org/teams/events/team_rankings/?RankSet=College-Men")
+# soup = BeautifulSoup(html.text, "html.parser")
+
+payload = {'CT_Main_0$lnkViewAll': ''}
+
+# file stuff
+output = open('games.csv', 'wb')
+writer = csv.writer(output, dialect='excel')
+writer.writerow( ('Team', 'opponent', 'score', 'date', 'tournament') )
+
+# make a request to the server
+r1 = requests.post("http://play.usaultimate.org/teams/events/team_rankings/?RankSet=College-Men", data=payload)
+soup1 = BeautifulSoup(r1.text, "html.parser")
+
+# get all the links in the HTML
+for HTMLLink in soup1.find_all('a'):
+    link = HTMLLink.get('href')
+
+    # if this is a link to a team then get the HTML there
+    if str(link).startswith('/teams/events/Eventteam'): 
+
+        teamHTML = requests.get("http://play.usaultimate.org" + str(link))
+        teamSoup = BeautifulSoup(teamHTML.text, "html.parser")
+
+        # get team info
+        teamName = teamSoup.find("div", class_="profile_info")
+        team = teamSoup.h4.text
+        teams = teamSoup.find_all(href=re.compile("TeamId"))
+        scores = teamSoup.find_all(href=re.compile("EventGame"))
+        dates = teamSoup.find_all(id=re.compile("lblMonth"))
+
+        tournament = ""
+        indexDifference = 0
+
+        # for every date get all the games
+        for index in range(len(dates)):
+            match = re.search(".* \d\d( |$)", dates[index].text)
+            if match:
+
+                writer.writerow( (team, \
+                                  teams[index - indexDifference].text, \
+                                  scores[index - indexDifference].text, \
+                                  dates[index].text, \
+                                  tournament \
+                               ) )
+
+                print("[%(team)s, %(opponent)s, %(score)s, " + \
+                      " %(date)s, %(tournament)s]" % \
+                     {"team": team, \
+                      "opponent":teams[index - indexDifference].text, \
+                      "score": scores[index - indexDifference].text, \
+                      "date": dates[index].text, \
+                      "tournament": tournament
+                     })
+
+            else:
+                indexDifference = indexDifference + 1
+                tournament = dates[index].text
+        print ""
